f4033dcf5a2cc1ca991ac0a7badfb9ef46e753aa
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-16 21:47:38 +0300

    task_28

--- .Program.cs
+++ .Program.cs
@@ -2,4 +2,4 @@
 
 var solution = new Solution();
 
-Console.WriteLine(solution.RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+Console.WriteLine(solution.StrStr("abc", "c"));
---
+++ FindTheIndexOfTheFirstOccurrenceInAString(28).cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack == needle)
+            {
+                return 0;
+            }
+
+            if (haystack.Length < needle.Length)
+            {
+                return -1;
+            }   
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0] && haystack.Length - i >= needle.Length)
+                {
+                    string subStr = haystack.Substring(i, needle.Length);
+
+                    if (subStr == needle)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
