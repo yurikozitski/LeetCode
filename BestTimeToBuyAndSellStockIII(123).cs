6fb02714150a58b789a24eea7074cb3a33db51ec
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-19 23:17:54 +0300

    task_112

---
+++ BestTimeToBuyAndSellStockIII(123).cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        //not working
+        public int MaxProfitIII(int[] prices)
+        {
+            if (prices.Length < 2)
+            {
+                return 0;
+            }
+
+            var profits = new List<int>();
+            int tempProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < prices[i - 1])
+                {
+                    profits.Add(tempProfit);
+                    tempProfit = 0;
+                }
+                else
+                {
+                    tempProfit += prices[i] - prices[i - 1];
+                }
+
+                if (i == prices.Length - 1)
+                {
+                    profits.Add(tempProfit);
+                }
+            }
+
+            if (profits.Count == 0)
+            {
+                return 0;
+            }
+
+            if (profits.Count == 1)
+            {
+                return profits[0];
+            }
+
+            return profits.OrderByDescending(n => n).Select(n => n).Take(2).Sum();
+        }
+    }
+}
---
+++ PathSum(112).cs
@@ -0,0 +1,195 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        // not working, has bug
+        //public bool HasPathSum(TreeNode root, int targetSum)
+        //{
+        //    if (root == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    int sum = 0;
+        //    var nodeStack = new Stack<TreeNode>();
+        //    TreeNode current = root;
+        //    nodeStack.Push(current);
+        //    sum += current.val;
+
+        //    while (true)
+        //    {
+        //        if (current.left != null)
+        //        {
+        //            current = current.left;
+        //            nodeStack.Push(current);
+        //            sum += current.val;
+        //        }
+        //        else
+        //        {
+        //            var currentTemp = nodeStack.Peek().right;
+
+        //            if (currentTemp != null)
+        //            {
+        //                current = currentTemp;
+        //                nodeStack.Push(current);
+        //                sum += current.val;
+        //                continue;
+        //            }
+        //            else
+        //            {
+        //                nodeStack.Pop();
+        //            }
+
+        //            if (sum == targetSum)
+        //            {
+        //                return true;
+        //            }
+
+        //            sum -= current.val;
+
+
+        //            while (true)
+        //            {
+        //                if (nodeStack.Count > 0)
+        //                {
+        //                    current = nodeStack.Peek();
+
+        //                    if (current.right == null)
+        //                    {
+        //                        nodeStack.Pop();
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    if (current.right == null)
+        //                    {
+        //                        sum -= current.val;
+        //                        break;
+        //                    }
+        //                }
+
+        //                if (current.right != null)
+        //                {
+        //                    var temp = current.right;
+        //                    current.right = null!;
+        //                    current = temp;
+        //                    nodeStack.Push(current);
+        //                    sum += current.val;
+        //                    break;
+        //                }
+        //                else
+        //                {
+        //                    sum -= current.val;
+        //                }
+        //            }
+        //        }
+
+        //        if (nodeStack.Count == 0)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
+
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            int sum = 0;
+            var nodeStack = new Stack<(TreeNode node, bool passed)>();
+            var current = (node: root, passed: false);
+            nodeStack.Push(current);
+            sum += current.node.val;
+
+            while (true)
+            {
+                if (current.node.left != null)
+                {
+                    current = (current.node.left, false);
+                    nodeStack.Push(current);
+                    sum += current.node.val;
+                }
+                else
+                {
+                    var currentTemp = nodeStack.Peek();
+
+                    if (currentTemp.node.right != null)
+                    {
+                        nodeStack.Pop();
+                        nodeStack.Push((currentTemp.node, true));
+
+                        current = (currentTemp.node.right, false);
+                        nodeStack.Push(current);
+                        sum += current.node.val;
+                        continue;
+                    }
+                    else
+                    {
+                        nodeStack.Pop();
+                    }
+
+                    if (sum == targetSum)
+                    {
+                        return true;
+                    }
+
+                    sum -= current.node.val;
+
+                    while (true)
+                    {
+                        if (nodeStack.Count > 0)
+                        {
+                            current = nodeStack.Peek();
+
+                            if (current.node.right == null || current.passed)
+                            {
+                                nodeStack.Pop();
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                        if (current.node.right != null && !current.passed)
+                        {
+                            nodeStack.Pop();
+                            nodeStack.Push((current.node, true));
+
+                            current = (current.node.right, false);
+                            nodeStack.Push(current);
+                            sum += current.node.val;
+                            break;
+                        }
+                        else
+                        {
+                            sum -= current.node.val;
+                        }
+                    }
+                }
+
+                if (nodeStack.Count == 0)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    static class TestDataForPathSum
+    {
+        public static TreeNode GetRoot1()
+        {
+            TreeNode root = new TreeNode(1);
+
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+
+
+            return root;
+        }
+    }
+}
