4efa25f6162f35d805bfccf6a8738b78ccebfd02
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-21 23:43:39 +0300

    task_137

---
+++ SingleNumberII(137).cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int SingleNumberII(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            Array.Sort(nums);
+
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                if (nums[i] != nums[i + 1] && nums[i] != nums[i - 1])
+                {
+                    return nums[i];
+                }
+            }
+
+            if (nums[0] != nums[1])
+            {
+                return nums[0];
+            }
+
+            if (nums[nums.Length - 1] != nums[nums.Length - 2])
+            {
+                return nums[nums.Length - 1];
+            }
+
+            return 0;
+        }
+    }
+}
