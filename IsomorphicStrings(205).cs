1469fbc74232f4f9e2b39fe7ec65dfed810df827
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-22 16:07:21 +0300

    task_205

---
+++ IsomorphicStrings(205).cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.ToCharArray().Distinct().Count() != t.ToCharArray().Distinct().Count())
+            {
+                return false;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j] && t[i] != t[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SingleNumberIII(260).cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int[] SingleNumberIII(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return [nums[0]];
+            }
+
+            Array.Sort(nums);
+            var result = new List<int>();
+
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                if (nums[i] != nums[i + 1] && nums[i] != nums[i - 1])
+                {
+                    result.Add(nums[i]);
+                }
+            }
+
+            if (nums[0] != nums[1])
+            {
+                result.Add(nums[0]);
+            }
+
+            if (nums[nums.Length - 1] != nums[nums.Length - 2])
+            {
+                result.Add(nums[nums.Length - 1]);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
