50b42c9aa22d897d165f900af4718cc90a048ca7
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-23 13:19:47 +0300

    task_191

---
+++ NumberOf1Bits(191).cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int HammingWeight(int n)
+        {
+            string binary = Convert.ToString(n, 2);
+            int count = 0;
+
+            foreach (var bit in binary)
+            {
+                if (bit == '1')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
