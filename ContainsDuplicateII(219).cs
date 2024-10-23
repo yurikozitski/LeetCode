a20f1abbd701e3cd5e1126789790c4178c4770c7
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-23 13:47:14 +0300

    task_219

---
+++ ContainsDuplicateII(219).cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && Math.Abs(i - j) <= k)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
