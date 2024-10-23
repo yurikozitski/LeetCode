ad7a02cb60baae6cd6be81406529a765c24b8dfd
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-23 13:25:33 +0300

    task_217

---
+++ ContainsDuplicate(217).cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            return nums.Length != nums.Distinct().Count();
+        }
+    }
+}
