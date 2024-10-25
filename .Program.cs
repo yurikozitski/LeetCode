be4b1d05f382f4dc8277c569b10713f5855935bd
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-25 16:14:40 +0300

    task_119

--- .Program.cs
+++ .Program.cs
@@ -2,4 +2,4 @@
 
 var solution = new Solution();
 
-Console.WriteLine(solution.Generate(5));
+Console.WriteLine(solution.GetRow(3));
---
+++ Pascal'sTriangleII(119).cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var result = new List<IList<int>>();
+
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                var row = new List<int>();
+
+                for (int j = 0; j <= i; j++)
+                {
+                    if (i == 0 || j == 0 || j == i)
+                    {
+                        row.Add(1);
+                    }
+                    else
+                    {
+                        int aboveLeft = result[i - 1][j - 1];
+                        int aboveRight = result[i - 1][j];
+
+                        row.Add(aboveLeft + aboveRight);
+                    }
+
+                }
+
+                result.Add(row);
+            }
+
+            return result[result.Count - 1];
+        }
+    }
+}
