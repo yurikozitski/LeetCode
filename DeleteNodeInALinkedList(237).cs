800d1f4a41b826f0995f98249a13ff8405c5f2b8
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-28 15:35:06 +0200

    task_237

---
+++ DeleteNodeInALinkedList(237).cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            while (node.next != null) 
+            {                
+                node.val = node.next.val;
+
+                if (node.next.next == null)
+                {
+                    node.next = null!;
+                }
+                else
+                {
+                    node = node.next;                
+                }
+            }
+        }
+    }
+}
