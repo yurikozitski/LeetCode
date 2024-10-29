b6e79877f355728087b2bb6a56d105b1424e1e0f
Юрій Козіцький <y.kozitski@gmail.com>
2024-10-29 13:59:18 +0200

    task_142

---
+++ LinkedListCycleII(142).cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var listSet = new HashSet<ListNode>();
+            ListNode current = head;
+
+            while (current != null)
+            {
+                if (listSet.Add(current))
+                {
+                    current = current.next;
+                }
+                else
+                {
+                    return current;
+                }
+            }
+
+            return null!;
+        }
+    }
+}
