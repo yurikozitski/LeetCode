22e0727e39ea03983233d2c32fee4f1a7c6d9b44
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-06 21:37:31 +0200

    task_349

---
+++ IntersectionOfTwoArrays(349).cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var num1Set = new HashSet<int>();
+            var result = new List<int>();
+
+            foreach (var num in nums1)
+            {
+                num1Set.Add(num);
+            }
+
+            result.AddRange(nums2.Where(num => num1Set.Remove(num)));
+
+            return result.ToArray();
+        }
+    }
+}
