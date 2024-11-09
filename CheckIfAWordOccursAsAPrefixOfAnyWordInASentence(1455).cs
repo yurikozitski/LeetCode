182daac7a14085f71ef421781fbc23ccad3afb11
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-09 19:04:32 +0200

    task_1455

---
+++ CheckIfAWordOccursAsAPrefixOfAnyWordInASentence(1455).cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            var sentenceArr = sentence.Split(' ');
+
+            for (int i = 0; i < sentenceArr.Length; i++)
+            {
+                if (sentenceArr[i].StartsWith(searchWord))
+                {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
