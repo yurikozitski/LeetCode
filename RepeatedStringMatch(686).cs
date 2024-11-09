86b5fa20b47f1971f9b9a2542fb1d26d5e3e13dc
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-09 15:01:15 +0200

    task_686

---
+++ RepeatedStringMatch(686).cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    partial class Solution
+    {
+        public int RepeatedStringMatch(string a, string b)
+        {
+            var aSb = new StringBuilder(a);
+            int count = 1;
+
+            while (aSb.Length < b.Length * 2 + a.Length * 2)
+            {
+                if (aSb.ToString().Contains(b))
+                {
+                    return count;
+                }
+
+                aSb.Append(a); 
+                count++;
+            }
+
+            return -1;
+        }
+    }
+}
