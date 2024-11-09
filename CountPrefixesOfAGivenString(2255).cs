e86af1dc081803e82537b70bb5cbacc2abec55d0
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-09 18:54:33 +0200

    task_2255

---
+++ CountPrefixesOfAGivenString(2255).cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    partial class Solution
+    {
+        public int CountPrefixes(string[] words, string s)
+        {
+            return words.Where(word => s.StartsWith(word)).Count();
+        }
+    }
+}
