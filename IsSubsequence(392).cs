c23ea0d8b29150cf36af7821616e6d1104c039d6
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-11 17:25:11 +0200

    task_392

---
+++ IsSubsequence(392).cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(t))
+            {
+                return false;
+            }
+
+            int i = 0;
+
+            foreach (var chr in t)
+            {
+                if (chr == s[i])
+                {
+                    i++;
+                }
+                
+                if (i == s.Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
