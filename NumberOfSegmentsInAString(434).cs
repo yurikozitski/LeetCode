baa00e2fea0d422eb4c5e2ae0eab1eb68523ffe9
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-13 20:29:59 +0200

    task_434

---
+++ NumberOfSegmentsInAString(434).cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int CountSegments(string s)
+        {
+            string sTrimmed = s.Trim();
+
+            if (string.IsNullOrEmpty(sTrimmed))
+            {
+                return 0;
+            }
+
+            bool isPreviousSpace = false;
+            int count = 0;
+
+            foreach (var chr in sTrimmed)
+            {
+                if (chr == ' ' && !isPreviousSpace)
+                {
+                    count++;
+                    isPreviousSpace = true;
+                }
+                else if (chr != ' ')
+                {
+                    isPreviousSpace = false;
+                }
+            }
+
+            return count + 1;
+        }
+    }
+}
