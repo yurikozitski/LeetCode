5fc0ca5742c2ff90327ce37edc5e850e16c15c11
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-13 21:14:57 +0200

    task_442

---
+++ FindAllDuplicatesInAnArray(442).cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var result = new List<int>();
+            var uniqueNums = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (!uniqueNums.Add(num))
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result;
+        }
+    }
+}
