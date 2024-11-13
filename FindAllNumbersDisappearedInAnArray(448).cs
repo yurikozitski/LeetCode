06167f22bf743c810a29b532eb20e972e0e75bb9
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-13 21:08:09 +0200

    task_448

---
+++ FindAllNumbersDisappearedInAnArray(448).cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            var result = new List<int>();
+            var uniqueNums = nums.ToHashSet();
+
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (!uniqueNums.Contains(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
