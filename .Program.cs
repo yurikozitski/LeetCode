23c40621bd30f26d79b806cfbe69cc5f6a097dc3
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-16 19:51:11 +0200

    task_482

--- .Program.cs
+++ .Program.cs
@@ -2,5 +2,5 @@
 
 var solution = new Solution();
 
-Console.WriteLine(solution.FindComplement(5));
+Console.WriteLine(solution.LicenseKeyFormatting("2-5g-3-J", 2));
 //solution.ReverseString(['h', 'e', 'l', 'l', 'o']);
---
+++ LicenseKeyFormatting(482).cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    partial class Solution
+    {
+        public string LicenseKeyFormatting(string s, int k)
+        {
+            s = s.Trim();
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            char[] sWithNoDashes = string.Join(string.Empty, s.Split('-')).ToUpper().ToCharArray();
+            int firstSegmentLength = sWithNoDashes.Length % k;
+            int chrCount = 0;
+            var result = new StringBuilder();
+
+            for (int i = 0; i < sWithNoDashes.Length; i++)
+            {
+                result.Append(sWithNoDashes[i]);
+                chrCount++;
+
+                if (i != sWithNoDashes.Length - 1 && ((firstSegmentLength != 0 && chrCount == firstSegmentLength) || chrCount == k))
+                {
+                    result.Append("-");
+                    chrCount = 0;
+                    firstSegmentLength = 0;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
