8356a7f6c6698a7074918c4c1c4dec914cf24b62
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-18 18:20:52 +0200

    task_485

---
+++ MaxConsecutiveOnes(485).cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int onesCount = 0;
+            int maxConsecutiveOnes = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    onesCount++;
+                }
+                else
+                {
+                    if (onesCount > maxConsecutiveOnes)
+                    {
+                        maxConsecutiveOnes = onesCount;
+                    }
+
+                    onesCount = 0;
+                }
+            }
+
+            return maxConsecutiveOnes > onesCount ? maxConsecutiveOnes : onesCount;
+        }
+    }
+}
