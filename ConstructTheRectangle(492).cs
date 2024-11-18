3556e7d3c89fda6811e1955947da9ddc47ccc1c9
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-18 19:11:23 +0200

    task_492

---
+++ ConstructTheRectangle(492).cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int[] ConstructRectangle(int area)
+        {
+            int[] result = new int[2];
+            double width = Math.Floor(Math.Sqrt((double)area));
+
+            while (width >= 1)
+            {
+                if (area % width == 0d)
+                {
+                    result[0] = (int)(area / width);
+                    result[1] = (int)width;
+                    break;
+                }
+
+                width--;
+            }
+
+            return result;
+        }
+    }
+}
