228eb1eace5a9cc06d30b0a1fdd20bbcd049497f
Юрій Козіцький <y.kozitski@gmail.com>
2024-11-20 15:39:22 +0200

    task_503

---
+++ NextGreaterElementII(503).cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int[] NextGreaterElements(int[] nums)
+        {
+            var result = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int j = i + 1;
+
+                while (true)
+                {
+                    if (j == nums.Length)
+                    {
+                        j = 0;
+                    }
+
+                    if (nums[j] > nums[i])
+                    {
+                        result.Add(nums[j]);
+                        break;
+                    }
+
+                    if (i == j)
+                    {
+                        result.Add(-1);
+                        break;
+                    }
+
+                    j++;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
