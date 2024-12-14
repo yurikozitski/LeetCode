cc0689007ff2d197e20c96a9113f72c70f2a72b9
Юрій Козіцький <y.kozitski@gmail.com>
2024-12-14 20:52:25 +0200

    task_520

---
+++ DetectCapital(520).cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.ToUpper() == word ||
+                word.ToLower() == word ||
+                word.Substring(1) == word.Substring(1).ToLower())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
