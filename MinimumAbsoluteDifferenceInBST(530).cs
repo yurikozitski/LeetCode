1777bb60337eb969ba7d7dbfd94eee4cdba49d7b
Юрій Козіцький <y.kozitski@gmail.com>
2024-12-16 19:32:49 +0200

    task_530

---
+++ MinimumAbsoluteDifferenceInBST(530).cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int GetMinimumDifference(TreeNode root)
+        {
+            var nodeValuesList = new List<int>();
+
+            var nodeQueue = new Queue<TreeNode>();
+            nodeQueue.Enqueue(root);
+
+            while (nodeQueue.Count != 0)
+            {
+                var currentNode = nodeQueue.Dequeue();
+
+                nodeValuesList.Add(currentNode.val);
+                
+                if (currentNode.left != null)
+                {
+                    nodeQueue.Enqueue(currentNode.left);
+                }
+
+                if (currentNode.right != null)
+                {
+                    nodeQueue.Enqueue(currentNode.right);
+                }
+            }
+
+            nodeValuesList.Sort();
+
+            int result = Math.Abs(nodeValuesList[0] - nodeValuesList[1]);
+
+            for (int i = 1; i < nodeValuesList.Count - 1; i++)
+            {
+                int currentDiff = Math.Abs(nodeValuesList[i] - nodeValuesList[i + 1]);
+
+                if (currentDiff < result)
+                {
+                    result = currentDiff;
+                }
+            }
+
+            return result;
+        }
+    }
+}
