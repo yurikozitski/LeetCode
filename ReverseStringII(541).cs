80fad377b466880662d05d7094f4ec3ee283dd4f
Юрій Козіцький <y.kozitski@gmail.com>
2024-12-25 14:58:39 +0200

    task_541

---
+++ ReverseStringII(541).cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    partial class Solution
+    {
+        public string ReverseStr(string s, int k)
+        {
+            int counter = 0;
+            bool isReversed = true;
+            var substringSb = new StringBuilder();
+            var resultSb = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                counter++;               
+                substringSb.Append(s[i]);
+
+                if (counter == k || i == s.Length - 1)
+                {
+                    if (isReversed)
+                    {
+                        string reversedSubStr = string.Join(string.Empty, substringSb.ToString().Reverse());
+                        resultSb.Append(reversedSubStr);                        
+                    }
+                    else
+                    {
+                        resultSb.Append(substringSb.ToString());
+                    }
+
+                    substringSb.Clear();
+                    counter = 0;
+                    isReversed = !isReversed;
+                }
+            }
+
+            return resultSb.ToString();
+        }
+    }
+}
