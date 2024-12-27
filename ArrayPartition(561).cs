8d953453082c1bf1b98dd706bff27da7f4159f1c
Юрій Козіцький <y.kozitski@gmail.com>
2024-12-27 14:37:11 +0200

    task_561

---
+++ ArrayPartition(561).cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int result = 0;
+
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                result += nums[i];
+            }
+
+            return result;
+        }
+    }
+}
