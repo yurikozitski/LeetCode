f0cac673dee5f0fd5184b0e8ac6deeeb8f1cc7a9
Юрій Козіцький <y.kozitski@gmail.com>
2024-12-28 15:34:44 +0200

    task_551

---
+++ StudentAttendanceRecordI(551).cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool CheckRecord(string s)
+        {
+            int absentCount = 0;
+            int lateInRowCount = 0;
+
+            foreach (var chr in s)
+            {
+                if (chr == 'A')
+                {
+                    absentCount++;
+                }
+
+                if (chr == 'L')
+                {
+                    lateInRowCount++;
+                }
+                else
+                {
+                    lateInRowCount = 0;
+                }
+
+                if (absentCount >= 2 || lateInRowCount >= 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
