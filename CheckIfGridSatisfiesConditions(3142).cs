a5a1c21046d15658e7e1bcfe2bbe84d77eb20377
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-02 20:08:06 +0200

    task_3142

---
+++ CheckIfGridSatisfiesConditions(3142).cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public bool SatisfiesConditions(int[][] grid)
+        {
+            for (int i = 0; i < grid[0].Length; i++)
+            {
+                for (int j = 0; j < grid.Length; j++)
+                {
+                    if (j + 1 != grid.Length && grid[j][i] != grid[j + 1][i] ||
+                        i + 1 != grid[j].Length && grid[j][i] == grid[j][i + 1])
+                    {
+                        return false;
+                    }                  
+                }
+            }
+
+            return true;
+        }
+    }
+}
