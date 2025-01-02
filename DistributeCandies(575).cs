eb9c8767fbf7bb5009e32570924f9d6a4ea96c08
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-02 19:21:11 +0200

    task_575

---
+++ DistributeCandies(575).cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int DistributeCandies(int[] candyType)
+        {
+            int numberOfTypes = candyType.Distinct().Count();
+            int numberOfCandies = candyType.Length;
+
+            if (numberOfTypes <= numberOfCandies / 2)
+            {
+                return numberOfTypes;
+            }
+            else
+            {
+                return numberOfCandies / 2;
+            }
+        }
+    }
+}
