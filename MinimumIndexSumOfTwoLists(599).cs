6a5a5fb29b916b857606a72f9cf5e35e0116f6f9
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-03 15:08:03 +0200

    task_599

---
+++ MinimumIndexSumOfTwoLists(599).cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            var list2Dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < list2.Length; i++)
+            {                
+                list2Dict[list2[i]] = i;                
+            }
+
+            List<(string Word, int IndexSum)> wordIndexList = new();
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                int list2Index = 0;
+
+                if (list2Dict.TryGetValue(list1[i], out list2Index))
+                {
+                    wordIndexList.Add((list1[i], i + list2Index));
+                }
+            }
+
+            wordIndexList = wordIndexList.OrderBy(x => x.IndexSum).ToList();
+
+            return wordIndexList
+                .Where(x => x.IndexSum == wordIndexList[0].IndexSum)
+                .Select(x => x.Word)
+                .ToArray();
+        }
+    }
+}
