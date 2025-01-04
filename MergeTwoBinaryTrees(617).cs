196bf14b6213958e7006824e787cabaf11b9f7c8
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-04 12:27:42 +0200

    task_617

---
+++ MergeTwoBinaryTrees(617).cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 != null && root2 != null)
+            {
+                root1.val += root2.val;
+
+                if (root1.left == null)
+                {
+                    root1.left = root2.left;
+                    root2.left = null;
+                }
+
+                if (root1.right == null)
+                {
+                    root1.right = root2.right;
+                    root2.right = null;
+                }
+            }
+            else if (root1 == null && root2 != null)
+            {
+                root1 = root2;
+            }
+
+            if (root1?.left != null)
+            {
+                MergeTrees(root1.left, root2?.left);
+            }
+
+            if (root1?.right != null)
+            {
+                MergeTrees(root1.right, root2?.right);
+            }
+
+            return root1;
+        }
+    }
+}
