a814413f11b9d2d3257fb901a6870891950b2c06
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-04 15:01:46 +0200

    task_628

---
+++ MaximumProductOfThreeNumbers(628).cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            int maxIndex = nums.Length - 1;
+
+            int res1 = nums[maxIndex] * nums[maxIndex - 1] * nums[maxIndex - 2];
+            int res2 = nums[0] * nums[1] * nums[maxIndex];
+
+            return int.Max(res1, res2);
+        }
+    }
+}
