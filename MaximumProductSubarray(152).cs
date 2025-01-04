e7550a8f4f31aa28ca4b6407b698fbe6173f5951
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-04 16:47:30 +0200

    task_152

---
+++ MaximumProductSubarray(152).cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        //don't pass, time limit exceeded
+        public int MaxProduct(int[] nums)
+        {
+            int product = int.MinValue;
+            int subArrayLength = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int firstIndex = 0;
+
+                while (firstIndex + subArrayLength <= nums.Length)
+                {
+                    int[] subArray = SubArray(nums, firstIndex, subArrayLength);
+                    int subArrayProduct = subArray.Aggregate(1, (acc, x) => acc * x);
+
+                    if (subArrayProduct > product)
+                    {
+                        product = subArrayProduct;
+                    }
+
+                    firstIndex++;
+                }
+
+                subArrayLength++;
+            }
+
+            int[] SubArray(int[] data, int index, int length)
+            {
+                int[] result = new int[length];
+                Array.Copy(data, index, result, 0, length);
+                return result;
+            }
+
+            return product;
+        }
+    }
+}
