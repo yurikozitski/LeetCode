ad406a1c628448e05f536a30d6b2da8ea06744eb
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-06 15:17:56 +0200

    task_637

---
+++ AverageOfLevelsInBinaryTree(637).cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            int level = 0;
+            var resultTable = new Dictionary<int, (int Number, long Sum)>();
+
+            void Traverse(TreeNode node)
+            {
+                if (resultTable.ContainsKey(level))
+                {
+                    var numberSumPair = resultTable[level];
+                    numberSumPair = (numberSumPair.Number += 1, numberSumPair.Sum += node.val);
+                    resultTable[level] = numberSumPair;
+                }
+                else
+                {
+                    resultTable[level] = (1, node.val);
+                }
+
+                if (node.left != null)
+                {
+                    level++;
+                    Traverse(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    level++;
+                    Traverse(node.right);
+                }
+
+                level--;
+            }
+
+            Traverse(root);
+
+            var result = new List<double>();
+
+            for (int i = 0; i < resultTable.Count; i++)
+            {
+                result.Add(Math.Round((double)resultTable[i].Sum / resultTable[i].Number, 5));
+            }
+
+            return result;
+        }
+    }
+}
