ba8037ec0918d26bd663bd745fcb213c1b2b48a5
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-06 15:47:36 +0200

    task_102

---
+++ BinaryTreeLevelOrderTraversal(102).cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            var nodeQueue = new Queue<(int Level, TreeNode Node)>();
+            nodeQueue.Enqueue((0, root));
+
+            while (nodeQueue.Count != 0)
+            {
+                var currentNodeTuple = nodeQueue.Dequeue();
+                int level = currentNodeTuple.Level;
+                var currentNode = currentNodeTuple.Node;
+
+                if (result.Count <= level)
+                {
+                    result.Add(new List<int>() { currentNode.val });
+                }
+                else
+                {
+                    result[level].Add(currentNode.val);
+                }
+
+                if (currentNode.left != null)
+                {
+                    nodeQueue.Enqueue((level + 1, currentNode.left));
+                }
+
+                if (currentNode.right != null)
+                {
+                    nodeQueue.Enqueue((level + 1, currentNode.right));
+                }
+            }
+
+            return result;
+        }
+    }
+}
