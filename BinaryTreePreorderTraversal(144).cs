20d0a750f4fe5afc5df22975602a525b5d764d16
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-10 20:58:45 +0200

    task_144

---
+++ BinaryTreePreorderTraversal(144).cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            Traverse(root);
+
+            void Traverse(TreeNode node)
+            {
+                result.Add(node.val);
+
+                if (node.left != null)
+                {
+                    Traverse(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    Traverse(node.right);
+                }
+            }
+
+            return result;
+        }
+    }
+}
