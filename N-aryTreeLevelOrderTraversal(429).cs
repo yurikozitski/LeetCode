f3e71ab3bae77f8d50f2ea3136b0c8b86a508059
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-10 20:18:40 +0200

    task_429

---
+++ N-aryTreeLevelOrderTraversal(429).cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+
+namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            var result = new List<IList<int>>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            var nodeQueue = new Queue<(int Level, Node Node)>();
+            nodeQueue.Enqueue((0, root));
+
+            while (nodeQueue.Count != 0)
+            {
+                var currentNodeTuple = nodeQueue.Dequeue();
+                int level = currentNodeTuple.Level;
+                var currentNode = currentNodeTuple.Node;
+
+                if (result.Count <= level)
+                {
+                    result.Add(new List<int>() { currentNode.val });
+                }
+                else
+                {
+                    result[level].Add(currentNode.val);
+                }
+
+                if (currentNode.children != null)
+                {
+                    foreach (var childNode in currentNode.children)
+                    {                        
+                        nodeQueue.Enqueue((level + 1, childNode));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ _Node.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
