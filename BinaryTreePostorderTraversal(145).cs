f5d58ce5b4ea3665eef698c0dd3d2c2512b2ffd3
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-10 21:26:11 +0200

    task_145

---
+++ BinaryTreePostorderTraversal(145).cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            Traverse(root);
+
+            void Traverse(TreeNode node)
+            {
+                if (node.left != null)
+                {
+                    Traverse(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    Traverse(node.right);
+                }
+
+                result.Add(node.val);
+            }
+
+            return result;
+        }
+    }
+}
