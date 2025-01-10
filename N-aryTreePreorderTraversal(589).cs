919db5969362f493e8ecf8ff6391815d137cf2ff
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-10 20:53:28 +0200

    task_589

---
+++ N-aryTreePreorderTraversal(589).cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> Preorder(Node root)
+        {
+            var result = new List<int>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            Traverse(root);
+
+            void Traverse(Node node)
+            {
+                result.Add(node.val);
+
+                if (node.children != null)
+                {
+                    foreach (var childNode in node.children)
+                    {
+                        Traverse(childNode);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
