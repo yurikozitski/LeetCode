2dc9547c3f55a48212177785e1b8f566e87637e5
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-10 21:23:22 +0200

    task_590

---
+++ N-aryTreePostorderTraversal(590).cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public IList<int> Postorder(Node root)
+        {
+            var result = new List<int>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            Traverse(root);
+
+            void Traverse(Node node)
+            {                
+                if (node.children != null)
+                {
+                    foreach (var childNode in node.children)
+                    {
+                        Traverse(childNode);
+                    }
+                }
+
+                result.Add(node.val);
+            }
+
+            return result;
+        }
+    }
+}
