44971227fed36267145a17e2cafa724b5755e25f
Юрій Козіцький <y.kozitski@gmail.com>
2025-01-15 17:47:13 +0200

    task_2520

---
+++ CountTheDigitsThatDivideA Number(2520).cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    partial class Solution
+    {
+        public int CountDigits(int num)
+        {
+            int res = 0;
+            int n = num;
+
+            while (n != 0)
+            {
+                int digit = n % 10;
+
+                if (!(digit == 0 || num % digit != 0))
+                {
+                    res++;
+                }
+
+                n /= 10;
+            }
+
+            return res;
+        }
+    }
+}
